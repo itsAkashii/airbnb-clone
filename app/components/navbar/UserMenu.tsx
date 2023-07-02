da020e5251b006384e7299433c29c2d170ab3f2f
itsAkashii <akashbirsingh622@gmail.com>
2023-07-01 22:00:59 -0700

    completed register functionality

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -5,9 +5,11 @@ import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/UseRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
 
 const UserMenu = () => {
 	const registerModal = useRegisterModal();
+	const loginModal = useLoginModal();
 
 	const [isOpen, setIsOpen] = useState(false);
 
@@ -40,7 +42,7 @@ const UserMenu = () => {
 					<div className='flex flex-col cursor-pointer'>
 						<>
 							<MenuItem
-								onClick={() => {}}
+								onClick={loginModal.onOpen}
 								label='Login'
 							/>
 							<MenuItem
