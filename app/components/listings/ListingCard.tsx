c4cdf2663ade56b869ba0d08a785b7147e13798c
itsAkashii <akashbirsingh622@gmail.com>
2023-08-16 13:32:13 -0700

    added favoriting functionality

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -8,6 +8,7 @@ import { useCallback, useMemo } from 'react';
 import format from 'date-fns/format';
 import Image from 'next/image';
 import HeartButton from '../HeartButton';
+import Button from '../Button';
 
 interface ListingCardProps {
 	data: Listing;
@@ -85,7 +86,25 @@ const ListingCard: React.FC<ListingCardProps> = ({
 						/>
 					</div>
 				</div>
+				<div className=' font-semibold text-lg'>
+					{location?.region}, {location?.label}
+				</div>
+				<div className=' font-light text-neutral-500'>
+					{reservationDate || data.category}
+				</div>
+				<div className=' flex flex-row items-center gap-1'>
+					<div className='font-semibold'>$ {price}</div>
+				</div>
+				{!reservation && <div className='font-light'>night</div>}
 			</div>
+			{onAction && actionLabel && (
+				<Button
+					disabled={disabled}
+					small
+					label={actionLabel}
+					onClick={handleCancel}
+				/>
+			)}
 		</div>
 	);
 };
